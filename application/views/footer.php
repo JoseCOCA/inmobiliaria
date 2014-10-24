38bfb9e9a6d8c6a32e6c62fe74853d9916a32e10
Jose Coca <josecoca0890@gmail.com>
2014-10-23 21:59:17 -0500

    footer con estilos

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,11 +2,18 @@
 <!--==============================footer=================================-->
 <footer> 
   <div class="container_12">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, voluptate veritatis porro, minima commodi esse. Commodi maiores repudiandae quisquam enim qui autem sed nisi odit facilis, ipsa illo magnam, praesentium!</p>
+    <p style = "padding-left: 40px; padding-right: 40px; margin-bottom: 70px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Asperiores vitae eos quia, quo numquam quae, earum deserunt similique perferendis 
+      libero fuga dolores odit. Labore earum, cum aperiam, eius expedita molestias?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Voluptatem, voluptate veritatis porro, minima commodi esse. 
+      Commodi maiores repudiandae quisquam enim qui autem sed nisi 
+      odit facilis, ipsa illo magnam, praesentium!</p>
   </div>  
   <div class="clear"></div>
   <div class="sep__1"></div>
-  <div class="foot_colourd"></div>
+  <div class="foot_colourd">
+  </div>
 </footer>
 <script type="text/javascript" src="assets/js/jquerypp.custom.js"></script>
     <script type="text/javascript" src="assets/js/jquery.elastislide.js"></script>
--- application/views/header.php
+++ application/views/header.php
@@ -97,7 +97,7 @@
   <div class="menu_block" >
     <nav class="horizontal-nav full-width horizontalNav-notprocessed">
       <ul class="sf-menu">
-       <li class="current"><a href="<?= base_url() ?>">INICIO</a></li>
+       <li><a href="<?= base_url() ?>">INICIO</a></li>
        <li><a href="<?= base_url("info") ?>">INFORMACIÓN DE EMPRESA</a></li>
        <li><a href="<?= base_url("privacy") ?>">AVISO DE PRIVACIDAD</a></li>
        <li><a href="<?= base_url("terms") ?>">TERMINOS DE USO</a></li>

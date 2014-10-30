295946570ecdacb9805797ac95fec0e6b0cf1d33
Jose Coca <josecoca0890@gmail.com>
2014-10-30 00:18:11 -0600

    estilos login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,26 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
-	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+	<style type="text/css">
+		*{
+			margin: 0px;
+			padding:0px;
+		}
+		form{
+			margin: 0 auto;
+			width: 200px;
+		}
+		body{
+			text-align: center;
+		}
+		#container_login{
+			margin: 10%;
+		}
+		.logo{
+			
+		}
+	</style>
 </head>
 <body>
 	<div id= "container_login">
-			<img src="" alt="">
+		<div class="logo">
+			<img src="assets/images/logo_inmobiliaria.png" alt="Logo">			
+		</div>
 			<h1>Login</h1>
 				<?php echo form_open('adminlogin');?>
-				<p>
-					<?php echo form_label('Email Address:', 'email_addres'); 
-						  echo form_input('email_address', set_value('email_address'), 'id="email_address"');
+				
+				<?php echo form_label('E-MAIL', 'email_addres'); 
+					 echo form_input('email_address', set_value('email_address'), 'id="email_address"');
 					?>
-				</p>
-				<p>
-					<?php echo form_label('Password:', 'password'); 
-						  echo form_password('password', '', 'id="password"');
-					?>
-				</p>
-				<p>
-					<?php echo form_submit('submit', 'Login'); ?>
-				</p>
+				<?php echo form_label('PASSWORD', 'password'); 
+					echo form_password('password', '', 'id="password"');?>
+				<?php echo form_submit('submit', 'LOGIN'); ?>
 				<?php echo form_close(); ?>
 
 			<div class="errors">
